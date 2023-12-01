160dfd38c37de16f1e846f25d755ba360c4455b5
Magh <63615725+Emgicraft@users.noreply.github.com>
2023-12-01 16:07:39 -0500

    Se añadieron métodos y constructores.
    Se definieron nuevos métodos para algunas operaciones específicas y un constructor adicional para mayor flexibilidad en la conexión.

--- src/modelo/SQLDB.java
+++ src/modelo/SQLDB.java
@@ -11,18 +11,30 @@ import java.util.logging.Logger;
  * @author Magh
  */
 public class SQLDB {
-    private final String databaseName = "DB_ElectronicAds";
+    private String databaseName = "DB_ElectronicAds";
     private final String user = "sa";
-    private final String password = "Magh26";
+    private final String password = "sa2996";
     private final String encrypt = "false";
     
+    /**
+     * Constructor vacío predeterminado.
+     */
     public SQLDB() {}
     
     /**
-     * Devuelve el objeto Connection en caso de establecerse la conexión, 
-     * de lo contrario, será nulo.
+     * Se específica el nombre de la base de datos a conectar.
      * 
-     * @return Instancia de conexión.
+     * @param nombreDB es el nombre de la base de datos.
+     */
+    public SQLDB(String nombreDB) {
+        this.databaseName = nombreDB;
+    }
+    
+    /**
+     * Devuelve el objeto <code>Connection</code> en caso de establecerse 
+     * la conexión, de lo contrario, será nulo.
+     * 
+     * @return <code>Connection</code> Instancia de conexión.
      */
     public Connection conectar() {
         Connection con = null;
@@ -34,5 +46,29 @@ public class SQLDB {
             Logger.getLogger(SQLDB.class.getName()).log(Level.SEVERE, null, ex);
         }
         return con;
+    }
+    
+    /**
+     * Devuelve un arreglo unidimensional con los nombres de 
+     * todas las tablas de la base de datos.
+     * 
+     * @return <code>String[]</code> con el nombre de todas las tablas de la BD.
+     */
+    public String[] getNombreTablas() {
+        String[] nombreTablas = {"1", "2"};
+        return nombreTablas;
+    }
+    
+    /**
+     * Devuelve un arreglo unidimensional con los nombres de 
+     * todos las campos de una tabla en específico.
+     * 
+     * @param nombreTabla Nombre de la tabla.
+     * @return <code>String[]</code> con el nombre de todos los campos de 
+     * la tabla especificada.
+     */
+    public String[] getCabeceraTabla(String nombreTabla) {
+        String[] cabeceraTabla = {nombreTabla};
+        return cabeceraTabla;
     }
 }
